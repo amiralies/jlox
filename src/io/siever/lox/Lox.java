9e273f3c8fd3714e05ce156296062e1dbbd6c933
Amirali Esmaeili <esmailiamirali@gmail.com>
2020-11-07 03:01:23 +0330

    Add parser

--- src/io/siever/lox/Lox.java
+++ src/io/siever/lox/Lox.java
@@ -49,11 +49,15 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    // Print toknes
-    for (Token token : tokens) {
-      System.out.println(token);
+    if (hadError) {
+      return;
     }
+
+    AstPrinter printer = new AstPrinter();
+    System.out.println(printer.print(expression));
   }
 
   static void error(int line, String message) {
@@ -63,5 +67,13 @@ public class Lox {
   private static void report(int line, String where, String message) {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 }
