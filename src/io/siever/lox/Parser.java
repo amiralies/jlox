c018ad730c66cf9fb010590bb1c89b7e45d1a40b
Amirali Esmaeili <esmailiamirali@gmail.com>
2020-11-13 21:14:56 +0330

    Add comma operator

--- src/io/siever/lox/Parser.java
+++ src/io/siever/lox/Parser.java
@@ -25,7 +25,19 @@ class Parser {
   }
 
   private Expr expression() {
-    return equality();
+    return commaSequence();
+  }
+
+  private Expr commaSequence() {
+    Expr expr = equality();
+
+    while (match(COMMA)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
   }
 
   private Expr equality() {
